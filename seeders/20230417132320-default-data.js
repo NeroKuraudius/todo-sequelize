8d78778236f03334e04b2f57104955b0876a27d5
Nero <karta0706566@gmail.com>
2023-04-17 21:37:44 +0800

    feat: add seed data

--- seeders/20230417132320-default-data.js
+++ seeders/20230417132320-default-data.js
@@ -1,26 +1,32 @@
 'use strict';
+const bcrypt = require('bcryptjs')
+const SEED_USER = {
+  name: 'root', email: 'root@exampale.com', password: '12345678'
+}
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    // bulkInsert: 大量資料寫入
+    return queryInterface.bulkInsert('Users', [{
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      // hashSync: hash的同步版本，genSaltSync亦同
+      password: bcrypt.hashSync(SEED_USER.password, bcrypt.genSaltSync(10), null),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {})
+      .then(userId => queryInterface.bulkInsert('Todos',
+        Array.from({ length: 10 }).map((_, i) => ({
+          name: `name-${i}`,
+          UserId: userId,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+        ), {}))
   },
-
+  // bulkDelete: 多筆資料刪除
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('Todos', null, {})
+      .then(() => queryInterface.bulkDelete('Users', null, {}))
   }
 };
