55665b56105f7f00332df029d91ecca639f98e55
Nero <karta0706566@gmail.com>
2023-04-18 21:19:33 +0800

    feat:add edit page

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -12,21 +12,61 @@ router.get('/new', (req, res) => {
 
 // 新增
 router.post('/new', (req, res) => {
-  const userId = req.user.id
+  const UserId = req.user.id
   const { name } = req.body
 
-  return Todo.create({ userId, name })
+  return Todo.create({ UserId, name })
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
 })
 
 // 查看詳細
 router.get('/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(Number(id))
+  const UserId = req.user.id
+  const id = Number(req.params.id)
+
+  return Todo.findOne({where:{UserId,id}})
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     // 上列程式碼等於Mongoose.findById().lean()
     .catch(error => console.log(error))
+})
+
+// 編輯頁面
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+
+  return Todo.findOne({ where: {UserId ,id} })
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+// 儲存編輯
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+
+  return Todo.findOne({ where: { UserId,id} })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      todo.updatedAt = new Date()
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(err => console.log(err))
+})
+
+// 刪除
+router.delete('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+
+  return Todo.findOne({ where: { UserId,id } })
+    .then(todo => todo.destroy())
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
 })
 
 module.exports = router
