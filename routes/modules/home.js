577671e33621cabe9d2eec2f985eab5f2053afb7
Nero <karta0706566@gmail.com>
2023-04-18 20:59:42 +0800

    feat: renew router & show home page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const router = express.Router()
 
+const db = require('../../models')
+const User = db.User
+const Todo = db.Todo
 
 router.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  }) // 上列程式碼等於Mongoose.find().lean()
-    .then((todos) => { return res.render('index', { todos: todos }) })
-    .catch((error) => { return res.status(422).json(error) })
+  User.findByPk(req.user.id)
+    .then((user) => {
+      if (!user) throw new Error('use not found.')
+
+      return Todo.findAll({
+        raw: true,
+        nest: true,
+        where: { UserId: req.user.id }
+      }) // 上列程式碼等於Mongoose.find().lean()
+    })
+    .then(todos => res.render('index', { todos }))
+    .catch(err=> console.log(err))
 })
 
 module.exports = router
