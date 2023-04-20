46286f2f6544fa890f5344b74558a78124d61480
Nero <karta0706566@gmail.com>
2023-04-20 10:36:02 +0800

    feat: add FB login function

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FBStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
@@ -8,21 +9,44 @@ const User = db.User
 module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+
+  passport.use(new LocalStrategy({ usernameField: 'email' ,passReqToCallback:true}, (req,email, password, done) => {
     User.findOne({ where: { email } })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: 'That email is not registered!' })
+          return done(null, false, req.flash('warning_msg', 'That email is not registered!' ))
         }
         return bcrypt.compare(password, user.password).then(isMatch => {
           if (!isMatch) {
-            return done(null, false, { message: 'Email or Password incorrect.' })
+            return done(null, false, req.flash('warning_msg', 'Email or Password incorrect.' ))
           }
           return done(null, user)
         })
       })
       .catch(err => done(err, false))
   }))
+
+  passport.use(new FBStrategy({
+    clientID: process.env.FB_ID,
+    clientSecret: process.env.FB_SECRET,
+    callbackURL: process.env.FB_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (user) return done(null, user)
+        const randomPassword = Math.random().toString(36).slice(-10)
+        bcrypt.genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name, email, password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(err => done(err))
+      })
+  }))
+
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
